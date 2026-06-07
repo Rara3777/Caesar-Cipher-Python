ce26cc2a05507e102a8eb7dc1e4a74522f40ea79
Ratnesh <ratneshwarveerappan@gmail.com>
2026-06-07 13:26:56 -0400

    Implement Caesar cipher for text encryption and decryption
    Add a simple encryption and decryption program using a Caesar cipher.

---
+++ main.py
@@ -0,0 +1,64 @@
+
+logo = r"""
+
+ ██████╗ █████╗ ███████╗███████╗ █████╗ ██████╗ 
+██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗
+██║     ███████║█████╗  ███████╗███████║██████╔╝
+██║     ██╔══██║██╔══╝  ╚════██║██╔══██║██╔══██╗
+╚██████╗██║  ██║███████╗███████║██║  ██║██║  ██║
+ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
+
+ ██████╗██╗██████╗ ██╗  ██╗███████╗██████╗ 
+██╔════╝██║██╔══██╗██║  ██║██╔════╝██╔══██╗
+██║     ██║██████╔╝███████║█████╗  ██████╔╝
+██║     ██║██╔═══╝ ██╔══██║██╔══╝  ██╔══██╗
+╚██████╗██║██║     ██║  ██║███████╗██║  ██║
+ ╚═════╝╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+
+
+"""
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        if letter not in alphabet:
+            cipher_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {cipher_text}")
+
+def decrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        if letter not in alphabet:
+            cipher_text += letter
+        else:
+            shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
+    print(f"Here is the decoded result: {cipher_text}")
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if direction == 'encode':
+        encrypt(original_text=text, shift_amount=shift)
+    elif direction == 'decode':
+        decrypt(original_text=text, shift_amount=shift)
+    else:
+        print("Please type 'encode' or 'decode'")
+    
+    user_input = input("Type 'Yes' to continue or 'No' to leave:\n").lower()
+    
+    if user_input != "yes":
+        should_continue = False
+        print("Bye")
